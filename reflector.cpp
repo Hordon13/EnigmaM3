c21fbe9c7a7d3c73ee9002c5869c27c34a3bd9e9
Hordon13 <hordonandron@gmail.com>
2019-05-12 02:12:39 +0200

    Create skeleton of the reflector class

--- reflector.cpp
+++ reflector.cpp
@@ -1,1 +1,15 @@
 #include "reflector.h"
+
+void Reflector::set(const std::string &type)
+{
+    if (type == "B") {
+        _wiring = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    } else if (type == "C") {
+        _wiring = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
+    }
+}
+
+int Reflector::reverse(int signal)
+{
+    return 0;
+}
--- reflector.h
+++ reflector.h
@@ -1,9 +1,15 @@
 #ifndef ENIGMAM3_REFLECTOR_H
 #define ENIGMAM3_REFLECTOR_H
 
+#include <string>
 
 class Reflector {
+public:
+    void set(const std::string &type);
 
+    int reverse(int signal);
+private:
+    std::string _wiring;
 };
 
 
