5517b64563d3b3de90b5b18d7f26e05d4216ada3
Hordon13 <hordonandron@gmail.com>
2019-05-12 19:27:32 +0200

    Add a basic console interface

--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@
 
 int main()
 {
-    Model enigma;
-    std::cout << "Enter a message: " << std::endl;
-    std::string input = "YKAENZAPMSCHZBFOCUVMRMDPYCOFHADZIZMEFXTHFLOLPZLFGGBOTGOXGRETDWTJIQHLMXVJWKZUASTR";
-    //std::cin >> input;
-    std::cout << enigma.encrypt(input) << std::endl;
+    std::cout << "Configure enigma... " << std::endl;
+    std::cout << "Example: 368/UZV/AHM/B/AN.EZ.HK.IJ.LR.MQ.OT.PV.SW.UX" << std::endl;
+    std::string config;
+    std::cin >> config;
+
+    if (config == ".") {
+        config = "368/UZV/AHM/B/AN.EZ.HK.IJ.LR.MQ.OT.PV.SW.UX";
+    }
+
+    Model enigma(config);
+
+    while (true) {
+        std::cout << std::endl;
+        std::cout << "Enter a message: ";
+        std::string input;
+        std::cin >> input;
+        if (input == "@exit") {
+            break;
+        }
+        std::cout << enigma.encrypt(input) << std::endl;
+    }
 
     return 0;
 }
