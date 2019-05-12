847812e821ec692d5859adb6911fea83b5efc884
Hordon13 <hordonandron@gmail.com>
2019-05-12 02:24:54 +0200

    Create skeleton of the plugboard class

--- plugboard.cpp
+++ plugboard.cpp
@@ -1,1 +1,17 @@
 #include "plugboard.h"
+
+void Plugboard::set(std::vector<std::pair<char, char>> plugPairs)
+{
+    for (auto &plugPair : plugPairs) {
+        _plugboard.insert(plugPair);
+    }
+}
+
+void Plugboard::transcript(std::string &message)
+{
+    for (char &i : message) {
+        if (_plugboard.find(i) != _plugboard.end()) {
+            i = _plugboard[i];
+        }
+    }
+}
--- plugboard.h
+++ plugboard.h
@@ -1,9 +1,17 @@
 #ifndef ENIGMAM3_PLUGBOARD_H
 #define ENIGMAM3_PLUGBOARD_H
 
+#include <string>
+#include <vector>
+#include <map>
 
 class Plugboard {
+public:
+    void set(std::vector<std::pair<char, char>> plugPairs);
 
+    void transcript(std::string &message);
+private:
+    std::map<char, char> _plugboard;
 };
 
 
