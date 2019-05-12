d44cc73467f9f6bc5863e752d07e5df4d3e7b829
Hordon13 <hordonandron@gmail.com>
2019-05-12 02:03:35 +0200

    Create skeleton of the rotor class

--- rotor.cpp
+++ rotor.cpp
@@ -1,1 +1,53 @@
 #include "rotor.h"
+
+Rotor::Rotor()
+{
+    _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    _ring = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+             'U', 'V', 'W', 'X', 'Y', 'Z'};
+    _core = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+             'U', 'V', 'W', 'X', 'Y', 'Z'};
+    _notch2 = -1;
+}
+
+void Rotor::setRotor(const std::string &type, const std::string &position, const std::string &ring)
+{
+    if (type == "1") {
+        _wiring = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+        _notch1 = 0;
+    } else if (type == "2") {
+        _wiring = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+        _notch1 = 0;
+    } else if (type == "3") {
+        _wiring = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+        _notch1 = 0;
+    } else if (type == "4") {
+        _wiring = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+        _notch1 = 0;
+    } else if (type == "5") {
+        _wiring = "VZBRGITYUPSDNHLXAWMJQOFECK";
+        _notch1 = 0;
+    } else if (type == "6") {
+        _wiring = "JPGVOUMFYQBENHZRDKASXLICTW";
+        _notch1 = 0;
+        _notch2 = 0;
+    } else if (type == "7") {
+        _wiring = "NZJHGRCXMYSWBOUFAIVLPEKQDT";
+        _notch1 = 0;
+        _notch2 = 0;
+    } else if (type == "8") {
+        _wiring = "FKQHTLXOCBJSPDZRAMEWNIUYGV";
+        _notch1 = 0;
+        _notch2 = 0;
+    }
+}
+
+void Rotor::turn()
+{
+
+}
+
+int Rotor::process(int signal)
+{
+    return 0;
+}
--- rotor.h
+++ rotor.h
@@ -2,9 +2,35 @@
 #define ENIGMAM3_ROTOR_H
 
 #include <string>
+#include <vector>
 
 class Rotor {
+public:
+    Rotor();
 
+    void setRotor(const std::string&, const std::string&, const std::string&);
+
+    void turn();
+
+    int process(int signal);
+
+private:
+    std::vector<char> _ring;
+    std::vector<char> _core;
+    std::string _wiring;
+
+    std::string _alphabet;
+    int _notch1;
+    int _notch2;
+
+    std::string _typeI;
+    std::string _typeII;
+    std::string _typeIII;
+    std::string _typeIV;
+    std::string _typeV;
+    std::string _typeVI;
+    std::string _typeVII;
+    std::string _typeVIII;
 };
 
 
